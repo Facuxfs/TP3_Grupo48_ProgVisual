ff5a320ed9663af4122517d0a0064d8b8edffa72
Facundo Romero <33964309+Facuxfs@users.noreply.github.com>
2022-04-20 11:04:43 -0300

    Creacion de Proyecto, 1 commit

---
+++ src/main/java/com/example/demo/Tp3Grupo48VisualApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3Grupo48VisualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3Grupo48VisualApplication.class, args);
+	}
+
+}

d4f0d3ebff2ebf1344bc5193823e4b2d1301b80d
Facundo Romero <33964309+Facuxfs@users.noreply.github.com>
2022-04-22 09:35:36 -0300

    Commit 4 : punto 5 ,6 y 7 tp3

--- src/main/java/ar/edu/unju/fi/ejercicio1/Principal.java
+++ src/main/java/ar/edu/unju/fi/ejercicio1/Principal.java
@@ -1,12 +1,13 @@
 package ar.edu.unju.fi.ejercicio1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
 	static int[] vector1 = new int[5];
 	static int[] vector2 = new int[5];
 	static int[] vectorParImpar = new int[7];
-	
+	ArrayList<String> frutas = new ArrayList<String>();
 	
 	public int[] cargarVectores (int[] vector) {
 		for (int i = 0; i < vector.length; i++) {
@@ -66,6 +67,56 @@ public class Principal {
 			}
 		}
 	}
+	public void saludarHolaMundo() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese su palabra");
+		String cadena = sc.next();
+		char[] saludo = cadena.toCharArray();
+		char[] invertido = new char[saludo.length];
+        for (int i=0; i<saludo.length; i++){
+        	invertido[i] = saludo[saludo.length-1-i];
+        	}
+        saludo=invertido;
+        for (int i=0; i<saludo.length; i++){
+        	System.out.println(saludo[i]);
+        }
+        }
+	
+	public void invertirDiezNumeros() {
+		Scanner sc = new Scanner(System.in);
+		int[] vectorNumero = new int[9];
+		for (int i = 0; i < vectorNumero.length; i++) {
+			System.out.println("Ingrese un Numero Entero");
+			vectorNumero[i] = sc.nextInt();
+		}
+		int[] invertido = new int[vectorNumero.length];
+        for (int i=0; i<vectorNumero.length; i++){
+        	invertido[i] = vectorNumero[vectorNumero.length-1-i];
+        	}
+        vectorNumero=invertido;
+        for (int i=0; i<vectorNumero.length; i++){
+        	System.out.println(vectorNumero[i]);
+        }
+	}
+	
+	public ArrayList<String> cargarFrutas(){ 
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> frutas = new ArrayList<String>();
+		do {
+		System.out.println("Ingrese una Fruta");
+		frutas.add(sc.next());
+		System.out.println("Desea agregar mas frutas? Y/N");
+		}while(sc.next().equals("Y")||(sc.next().equals("y")));
+		
+		return frutas;
+	}
+	
+	public void mostrarFrutas(ArrayList<String> frutas) {
+		for(String fruta: frutas) {
+			System.out.println(fruta);
+		}
+		
+	}
 	
 	
 	public static void main(String[] args) {
@@ -81,11 +132,20 @@ public class Principal {
 		//prueba.cargarTablaMultiplicar();
 		
 		//Punto 3
-		Alumno unAlumno = new Alumno();
-		unAlumno.cargarDatosAlumno(unAlumno);
+		//Alumno unAlumno = new Alumno();
+		//unAlumno.cargarDatosAlumno(unAlumno);
 		
 		//Punto 4
-		prueba.numerosParImpar();
+		//prueba.numerosParImpar();
+		
+		//Punto5
+		//prueba.saludarHolaMundo();
+		
+		//Punto6
+		//prueba.invertirDiezNumeros();
+		
+		//Punto 7 
+		prueba.mostrarFrutas(prueba.cargarFrutas());
 	}
 
 }
